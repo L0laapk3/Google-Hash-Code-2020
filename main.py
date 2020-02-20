751b540d1ea4ef221cd67be5e0c64d9e47b3fb4c
L0laapk3 <L0laapk3@gmail.com>
2020-02-20 20:39:43 +0100

    pre rarity delete

--- main.py
+++ main.py
@@ -2,94 +2,168 @@ import sys
 import itertools
 import multiprocessing as mp
 
-if len(sys.argv) > 1:
-	infile = sys.argv[1] + ".txt"
-else:
-	infile = "b.txt"
-
-
 
 
 
 PRINTBOOKS = False
 
-class Lib:
-	def __init__(self, index, signupDays, troughput, books):
-		self.index = index
-		self.signupDays = signupDays
-		self.troughput = troughput
-		self.books = books
+def main(infile):
+
+
+
+	class Book:
+		def __init__(self, index, score):
+			self.index = index
+			self.originalIndex = index
+			self.score = score
+			self.rarity = 1
+
+
+
+		def __repr__(self):
+			return self.__str__()
+		def __str__(self):
+			stuff = dict(vars(self))
+			if not PRINTBOOKS:
+				stuff["books"] = f"len({len(stuff['books'])}"
+			for k in stuff:
+				if isinstance(stuff[k], float):
+					stuff[k] = round(stuff[k], 2)
+			return str(stuff)
+
+
+
+	class Lib:
+		def __init__(self, index, signupDays, troughput, books):
+			self.originalIndex = index
+			self.index = index
+			self.signupDays = signupDays
+			self.troughput = troughput
+
+			self.books = books
+			self.booksByIndex = dict()
+			for bookI, book in enumerate(books):
+				self.booksByIndex[book.index] = bookI
+			
+
+			
+		def __repr__(self):
+			return self.__str__()
+		def __str__(self):
+			stuff = dict(vars(self))
+			if not PRINTBOOKS:
+				stuff["books"] = f"len({len(stuff['books'])}"
+			for k in stuff:
+				if isinstance(stuff[k], float):
+					stuff[k] = round(stuff[k], 2)
+			return str(stuff)
+
+
+
+	with open(infile, 'r') as inf:
+
+		infoLine = inf.readline()
+		info = infoLine.split(" ")
 		
-
+		numBooks = int(info[1])
+		numLib = int(info[1])
 		
-	def __repr__(self):
-		return self.__str__()
-	def __str__(self):
-		stuff = dict(vars(self))
-		if not PRINTBOOKS:
-			stuff["books"] = f"len({len(stuff['books'])}"
-		for k in stuff:
-			if isinstance(stuff[k], float):
-				stuff[k] = round(stuff[k], 2)
-		return str(stuff)
+		bookToLib = list(map(lambda x: list(), [None] * numBooks))
+
+		deadline = int(info[2])
+
+		allBooks = list(map(Book, enumerate(inf.readline().split(" "))))
+
+		libs = []
+		for libIndex in range(0, numLib):
+			libInfo = inf.readline().split(" ")
+
+			books = map(lambda x: allBooks[int(x)], inf.readline().split(" "))
+			for book in books:
+				book.rarity += 1
+				bookToLib[book.index].append(libIndex)
+
+			lib = Lib(libIndex, int(libInfo[1]), int(libInfo[2]), list(books))
+			libs.append(lib)
 
 
 
-with open(infile, 'r') as inf:
-
-	infoLine = inf.readline()
-	info = infoLine.split(" ")
-	
-	numBooks = int(info[0])
-	numLib = int(info[1])
-	deadline = int(info[2])
-
-	bookScores = tuple(map(lambda x: int(x), inf.readline().split(" ")))
-
-	libs = []
-	for libIndex in range(0, numLib):
-		libInfo = inf.readline().split(" ")
-		lib = Lib(libIndex, int(libInfo[1]), int(libInfo[2]), list(map(lambda x: int(x), inf.readline().split(" "))))
-		libs.append(lib)
-
-
-# print(deadline)
-# print(libs)
-# print(bookScores)
-
-
-bookRarity = [1] * len(bookScores)
-bookValue = [None] * len(bookScores)
-for lib in libs:
-	for bookI in lib.books:
-		bookRarity[bookI] += 1
+	# print(deadline)
+	# print(libs)
+	# print(bookScores)
 
 
 
-for bookI in range(len(bookScores)):
-	bookValue[bookI] = bookScores[bookI] / bookRarity[bookI]
 
-for lib in libs:
-	lib.value = 0
-	for bookI in lib.books:
-		lib.value += bookValue[bookI]
-
-	lib.weight = lib.value / lib.signupDays
-
-	lib.books.sort(key=lambda b: bookValue[b], reverse=True)
+	# initial book values berekenen
+	for book in allBooks:
+		book.value = book.score / book.rarity
 
 
+	for lib in libs:
+		# initial sort
+		lib.books.sort(key=lambda b: b.value, reversed=True)
+		
+		# initial lib value
+		lib.value = 0
+		for book in lib.books[0:(deadline - lib.signupDays)*lib.troughput]:
+			lib.value += book.value
 
-libs.sort(key=lambda x: x.weight, reverse=True)
+			
+		# initial lib book sort order berekenen
 
 
-print(libs)
+	solution = []
+	while deadline < 0:
 
-solution = libs
+
+		# 1. beste lib vinden
+		bestLib = None
+		bestLibScore = 0
+		for lib in libs:
+
+			# TODO: rekening houden met deadline
+			libScore = lib.value / lib.signupDays
+			if libScore > bestLibScore:
+				bestLibScore = libScore
+				bestLib = lib
+		
+		solution.append(lib)
 
 
-with open("out" + infile, "w") as txt_file:
-    txt_file.write(str(len(solution)))
-    for lib in solution:
-        txt_file.write(f"\n{lib.index} {len(lib.books)}")
-        txt_file.write("\n" + " ".join(map(str, lib.books)) )
+
+		# 2. lib eruithalen
+		libs[bestLib.index] = libs.pop()
+		libs[bestLib.index].index = bestLib.index
+
+
+		for book in bestLib.books:
+
+			# 4. boeken er uit alle andere libs uithalen
+			for lib in bookToLib[book.index]:
+				bookI = lib.booksByIndex[book.index]
+				removedBook = lib.books.pop(bookI)
+				lib.value -= removedBook
+
+				# moet ook nog boeken in libs hersorteren
+
+
+				
+			# 3. book rarities aanpassen, zou eigenlijk nie nodig moeten zijn..
+			book.score = 0
+			book.value = 0
+
+
+
+
+
+	print(libs)
+
+	solution = libs
+
+
+	with open("out" + infile, "w") as txt_file:
+		txt_file.write(str(len(solution)))
+		for lib in solution:
+			txt_file.write(f"\n{lib.originalIndex} {len(lib.books)}")
+			txt_file.write("\n" + " ".join(map(lambda x: str(x.originalIndex), lib.books)) )
